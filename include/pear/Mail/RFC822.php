plch/osTicket

commit f99ca0c4142538ae4861d27892118b4713e944a7

Brush up import concept to be more flexible

include/pear/Mail/RFC822.php
@@ -362,13 +362,17 @@ class Mail_RFC822 {
      */
     function _hasUnclosedQuotes($string)
     {
+        $matches = array();
+        if (!preg_match_all('/\\|"/S', $string, $matches, PREG_SET_ORDER))
+            return false;
+
         $string = trim($string);
         $iMax = strlen($string);
         $in_quote = false;
         $i = $slashes = 0;
 
-        for (; $i < $iMax; ++$i) {
-            switch ($string[$i]) {
+        foreach ($matches[0] as $m) {
+            switch ($m) {
             case '\\':
                 ++$slashes;
                 break;
@@ -425,7 +429,7 @@ class Mail_RFC822 {
     function _hasUnclosedBracketsSub($string, &$num, $char)
     {
         $parts = explode($char, $string);
-        for ($i = 0; $i < count($parts); $i++){
+        for ($i = 0, $k = count($parts); $i < $k; $i++){
             if (substr($parts[$i], -1) == '\\' || $this->_hasUnclosedQuotes($parts[$i]))
                 $num--;
             if (isset($parts[$i + 1]))

setup/cli/modules/import.php
@@ -14,6 +14,9 @@
     vim: expandtab sw=4 ts=4 sts=4:
 **********************************************************************/
 require_once dirname(__file__) . "/class.module.php";
+require_once dirname(__file__) . "../../cli.inc.php";
+require_once INCLUDE_DIR . "class.export.php";
+require_once INCLUDE_DIR . 'class.json.php';
 
 class Importer extends Module {
     var $prologue =
@@ -33,6 +36,8 @@ class Importer extends Module {
             tables. This option can be specified more than once"),
         'drop' => array('-D', '--drop', 'action'=>'store_true', 'help'=>
             'Issue DROP TABLE statements before the create statemente'),
+        'go-baby' => array('-P', '--prime-time', 'action'=>'store_true',
+            'help'=>'Load data into live database. Use at your own risk'),
     );
 
     var $epilog =
@@ -41,11 +46,46 @@ class Importer extends Module {
     var $stream;
     var $header;
     var $source_ost_info;
+    var $parent;
+
+    function __construct($parent=false) {
+        parent::__construct();
+        if ($parent) {
+            $this->parent = $parent;
+            $this->stream = $parent->stream;
+        }
+    }
+    function __get($what) {
+        return $this->parent->{$what};
+    }
+    function __call($what, $how) {
+        return call_user_func_array(array($this->parent, $what), $how);
+    }
+
+    function run($args, $options) {
+        $stream = $options['stream'];
+        if ($options['compress']) $stream = "compress.zlib://$stream";
+        if (!($this->stream = fopen($stream, 'rb'))) {
+            $this->stderr->write('Unable to open input stream');
+            die();
+        }
+
+        if (!$this->verify_header())
+            die('Unable to verify backup header');
+
+        Bootstrap::connect();
+
+        $class = 'Importer_' . $this->header[1];
+        $importer = new $class($this);
+
+        while ($importer->import_table());
+        @fclose($this->stream);
+    }
 
     function verify_header() {
         list($header, $info) = $this->read_block();
         if (!$header || $header[0] != OSTICKET_BACKUP_SIGNATURE) {
-            $this->stderr->write('Header mismatch -- not an osTicket backup');
+            $this->stderr->write("Header mismatch -- not an osTicket backup\n");
             return false;
         }
         else
@@ -60,9 +100,7 @@ class Importer extends Module {
     }
 
     function read_block() {
-        $block = '';
-        while (!feof($this->stream) && (($c = fgetc($this->stream)) != "\x1e"))
-            $block .= $c;
+        $block = fgets($this->stream);
 
         if ($json = JsonDataParser::decode($block))
             return $json;
@@ -70,6 +108,37 @@ class Importer extends Module {
         if (strlen($block)) {
             $this->stderr->write("Unable to read block from input");
             die();
+        }
+    }
+
+    function load_row($info, $row, $flush=false) {
+        static $header = null;
+        static $rows = array();
+        static $length = 0;
+
+        if ($info && $header === null) {
+            $header = "INSERT INTO `".TABLE_PREFIX.$info[1].'` (';
+            $cols = array();
+            foreach ($info[2] as $col)
+                $cols[] = "`{$col['Field']}`";
+            $header .= implode(', ', $cols);
+            $header .= ") VALUES ";
+        }
+        if ($row) {
+            $values = array();
+            foreach ($info[2] as $i=>$col)
+                $values[] = (is_numeric($row[$i]))
+                    ? $row[$i]
+                    : ($row[$i] ? '0x'.bin2hex($row[$i]) : "''");
+            $values = "(" . implode(', ', $values) . ")";
+            $length += strlen($values);
+            $rows[] = &$values;
+        }
+        if (($flush || $length > 16000) && $header) {
+            $this->send_statement($header . implode(',', $rows));
+            $header = null;
+            $rows = array();
+            $length = 0;
         }
     }
 
@@ -81,6 +150,9 @@ class Importer extends Module {
             $this->stdout->write(";\n");
         }
     }
+}
+
+class Importer_A extends Importer {
 
     function import_table() {
         if (!($header = $this->read_block()))
@@ -173,18 +245,44 @@ class Importer extends Module {
                 .')');
         }
     }
+}
 
+class Importer_B extends Importer {
     function truncate_table($info) {
         $this->send_statement('TRUNCATE TABLE '.TABLE_PREFIX.$info[1]);
-        $indexes = array();
-        foreach ($info[3] as $idx) {
-            if ($idx['Key_name'] == 'PRIMARY')
-                continue;
-            $indexes[$idx['Key_name']] =
-                '`'.TABLE_PREFIX.$info[1].'`.`'.$idx['Key_name'].'`';
+    }
+
+    function import_table() {
+        if (!($header = $this->read_block()))
+            return false;
+
+        else if ($header[0] != 'table') {
+            $this->stderr->write('Unable to read table header');
+            return false;
         }
-        foreach ($indexes as $T=>$fqn)
-            $this->send_statement('DROP INDEX IF EXISTS '.$fqn);
+
+        // TODO: Consider included tables and excluded tables
+
+        $this->stderr->write("Importing table: {$header[1]}\n");
+
+        $res = db_query("select column_name from information_schema.columns
+            where table_schema=DATABASE() and table_name='$t'");
+        while (list($field) = db_fetch_row($res))
+            if (!in_array($field, $header[2]))
+                $this->fail($header[1]
+                    .": Destination table does not have the `$field` field");
+
+        if (!isset($header[3]['truncate']) || $header[3]['truncate'])
+            $this->truncate_table($header);
+
+        while (($row=$this->read_block())) {
+            if (isset($row[0]) && ($row[0] == 'end-table')) {
+                $this->load_row(null, null, true);
+                return true;
+            }
+            $this->load_row($header, $row);
+        }
+        return false;
     }
 
     function load_row($info, $row, $flush=false) {
@@ -196,7 +294,7 @@ class Importer extends Module {
             $header = "INSERT INTO `".TABLE_PREFIX.$info[1].'` (';
             $cols = array();
             foreach ($info[2] as $col)
-                $cols[] = "`{$col['Field']}`";
+                $cols[] = "`$col`";
             $header .= implode(', ', $cols);
             $header .= ") VALUES ";
         }
@@ -218,23 +316,6 @@ class Importer extends Module {
         }
     }
 
-    function run($args, $options) {
-        require_once dirname(__file__) . '/../../../main.inc.php';
-        require_once INCLUDE_DIR . 'class.json.php';
-
-        $stream = $options['stream'];
-        if ($options['compress']) $stream = "compress.zlib://$stream";
-        if (!($this->stream = fopen($stream, 'rb'))) {
-            $this->stderr->write('Unable to open input stream');
-            die();
-        }
-
-        if (!$this->verify_header())
-            die('Unable to verify backup header');
-
-        while ($this->import_table());
-        @fclose($this->stream);
-    }
 }
 
 Module::register('import', 'Importer');

include/class.export.php
@@ -213,7 +213,7 @@ class ResultSetExporter {
         $this->options = $options;
         $this->output = $options['output'] ?: fopen('php://output', 'w');
 
-        $this->_res = db_query($sql);
+        $this->_res = db_query($sql, true, true);
         if ($row = db_fetch_array($this->_res)) {
             $query_fields = array_keys($row);
             $this->headers = array();
@@ -296,9 +296,13 @@ require_once INCLUDE_DIR . 'class.json.php';
 require_once INCLUDE_DIR . 'class.migrater.php';
 require_once INCLUDE_DIR . 'class.signal.php';
 
+define('OSTICKET_BACKUP_SIGNATURE', 'osTicket-Backup');
+define('OSTICKET_BACKUP_VERSION', 'B');
+
 class DatabaseExporter {
 
     var $stream;
+    var $options;
     var $tables = array(CONFIG_TABLE, SYSLOG_TABLE, FILE_TABLE,
         FILE_CHUNK_TABLE, STAFF_TABLE, DEPT_TABLE, TOPIC_TABLE, GROUP_TABLE,
         GROUP_DEPT_TABLE, TEAM_TABLE, TEAM_MEMBER_TABLE, FAQ_TABLE,
@@ -311,21 +315,20 @@ class DatabaseExporter {
         TIMEZONE_TABLE, SESSION_TABLE, PAGE_TABLE,
         FORM_SEC_TABLE, FORM_FIELD_TABLE, LIST_TABLE, LIST_ITEM_TABLE,
         FORM_ENTRY_TABLE, FORM_ANSWER_TABLE, USER_TABLE, USER_EMAIL_TABLE,
+        PLUGIN_TABLE, TICKET_COLLABORATOR_TABLE
     );
 
-    function DatabaseExporter($stream) {
+    function DatabaseExporter($stream, $options=array()) {
         $this->stream = $stream;
+        $this->options = $options;
     }
 
     function write_block($what) {
         fwrite($this->stream, JsonDataEncoder::encode($what));
-        fwrite($this->stream, "\x1e");
+        fwrite($this->stream, "\n");
     }
 
-    function dump($error_stream) {
-        // Allow plugins to change the tables exported
-        Signal::send('export.tables', $this, $this->tables);
-
+    function dump_header() {
         $header = array(
             array(OSTICKET_BACKUP_SIGNATURE, OSTICKET_BACKUP_VERSION),
             array(
@@ -338,21 +341,22 @@ class DatabaseExporter {
             ),
         );
         $this->write_block($header);
+    }
+
+    function dump($error_stream) {
+        // Allow plugins to change the tables exported
+        Signal::send('export.tables', $this, $this->tables);
+        $this->dump_header();
 
         foreach ($this->tables as $t) {
             if ($error_stream) $error_stream->write("$t\n");
+
             // Inspect schema
-            $table = $indexes = array();
-            $res = db_query("show columns from $t");
-            while ($field = db_fetch_array($res))
+            $table = array();
+            $res = db_query("select column_name from information_schema.columns
+                where table_schema=DATABASE() and table_name='$t'");
+            while (list($field) = db_fetch_row($res))
                 $table[] = $field;
-
-            $res = db_query("show indexes from $t");
-            while ($col = db_fetch_array($res))
-                $indexes[] = $col;
-
-            $res = db_query("select * from $t");
-            $types = array();
 
             if (!$table) {
                 if ($error_stream) $error_stream->write(
@@ -360,8 +364,9 @@ class DatabaseExporter {
                 die();
             }
             $this->write_block(
-                array('table', substr($t, strlen(TABLE_PREFIX)), $table,
-                    $indexes));
+                array('table', substr($t, strlen(TABLE_PREFIX)), $table));
+
+            db_query("select * from $t");
 
             // Dump row data
             while ($row = db_fetch_row($res))

include/i18n/en_US/form.yaml
@@ -78,7 +78,8 @@
       tickets, and will be searchable with advanced search and filterable.
   deletable: false
   fields:
-    - type: text # notrans
+    - id: 20
+      type: text # notrans
       name: subject # notrans
       label: Issue Summary
       required: true
@@ -88,7 +89,8 @@
         size: 40
         length: 50
 
-    - type: thread # notrans
+    - id: 21
+      type: thread # notrans
       name: message # notrans
       label: Issue Details
       hint: Details on the reason(s) for opening the ticket.
@@ -96,7 +98,8 @@
       edit_mask: 15
       sort: 2
 
-    - type: priority # notrans
+    - id: 22
+      type: priority # notrans
       name: priority # notrans
       label: Priority Level
       required: false

include/mysqli.php
@@ -143,12 +143,18 @@ function db_create_database($database, $charset='utf8',
  * (mixed) MysqliResource if SELECT query succeeds, true if an INSERT,
  * UPDATE, or DELETE succeeds, false or null if the query fails.
  */
-function db_query($query, $logError=true) {
+function db_query($query, $logError=true, $buffered=true) {
     global $ost, $__db;
+
+    if ($__db->unbuffered_result) {
+        $__db->unbuffered_result->free();
+        $__db->unbuffered_result = false;
+    }
 
     $tries = 3;
     do {
-        $res = $__db->query($query);
+        $res = $__db->query($query,
+            $buffered ? MYSQLI_STORE_RESULT : MYSQLI_USE_RESULT);
         // Retry the query due to deadlock error (#1213)
         // TODO: Consider retry on #1205 (lock wait timeout exceeded)
         // TODO: Log warning
@@ -163,6 +169,9 @@ function db_query($query, $logError=true) {
         $ost->logDBError('DB Error #'.db_errno(), $msg);
         //echo $msg; #uncomment during debuging or dev.
     }
+
+    if (is_object($res) && !$buffered)
+        $__db->unbuffered_result = $res;
 
     return $res;
 }
@@ -182,11 +191,13 @@ function db_count($query) {
     return db_result(db_query($query));
 }
 
-function db_result($res, $row=0) {
+function db_result($res, $row=false) {
     if (!$res)
         return NULL;
 
-    $res->data_seek($row);
+    if ($row !== false)
+        $res->data_seek($row);
+
     list($value) = db_output($res->fetch_row());
     return $value;
 }

setup/cli/modules/export.php
@@ -14,9 +14,7 @@
     vim: expandtab sw=4 ts=4 sts=4:
 **********************************************************************/
 require_once dirname(__file__) . "/class.module.php";
-
-define('OSTICKET_BACKUP_SIGNATURE', 'osTicket-Backup');
-define('OSTICKET_BACKUP_VERSION', 'A');
+require_once dirname(__file__) . "../../cli.inc.php";
 
 class Exporter extends Module {
     var $prologue =
@@ -30,18 +28,62 @@ class Exporter extends Module {
             'help'=> "Send zlib compress data to the output stream"),
     );
 
+    var $arguments = array(
+        'module' => array(
+            'required' => false,
+            'help' => 'Module used for export (see help)'
+        ),
+    );
+
+    var $autohelp = false;
+
     function run($args, $options) {
-        require_once dirname(__file__) . '/../../../main.inc.php';
+        require_once dirname(__file__) . '/../../../bootstrap.php';
         require_once INCLUDE_DIR . 'class.export.php';
 
-        global $ost;
+        if (!$args['module']) {
+            $exporter = 'DatabaseExporter';
+        }
+        else {
+            $module = (include dirname(__file__)."/importer/{$args['module']}.php");
+            if ($module) {
+                $module = new $module();
+                return $module->_run($args['module']);
+            }
+            else {
+                $this->stderr->write("Unknown importer module given\n");
+                $this->showHelp();
+            }
+        }
+        if ($exporter)
+            $this->dump($exporter);
+    }
 
-        $stream = $options['stream'];
-        if ($options['compress']) $stream = "compress.zlib://$stream";
+    function dump($module) {
+        $stream = $this->getOption('stream');
+        if ($this->getOption('compress')) $stream = "compress.zlib://$stream";
         $stream = fopen($stream, 'w');
 
-        $x = new DatabaseExporter($stream);
+        $x = new $module($stream, $this->_options);
         $x->dump($this->stderr);
+    }
+
+    function showHelp() {
+        $modules = array();
+        foreach (glob(dirname(__file__).'/importer/*.php') as $script) {
+            $info = pathinfo($script);
+            $modules[] = $info['filename'];
+        }
+
+        $this->epilog =
+            "Currently available modules follow. Use 'manage.php export <module>
+            --help' for usage regarding each respective module:";
+
+        parent::showHelp();
+
+        echo "\n";
+        foreach ($modules as $name)
+            echo str_pad($name, 20) . "\n";
     }
 }
 

setup/cli/modules/file.php
@@ -14,6 +14,7 @@ class FileManager extends Module {
                 'dump' => 'Dump file content to stdout',
                 'migrate' => 'Migrate a file to another backend',
                 'backends' => 'List configured storage backends',
+                'expunge' => 'Remove matching files from the system',
             ),
         ),
     );
@@ -21,7 +22,7 @@ class FileManager extends Module {
     var $options = array(
         'ticket' => array('-T', '--ticket', 'metavar'=>'id',
             'help' => 'Search by internal ticket id'),
-        'file' => array('-F', '--file', 'metavar'=>'id',
+        'file-id' => array('-F', '--file-id', 'metavar'=>'id',
             'help' => 'Search by file id'),
         'name' => array('-N', '--name', 'metavar'=>'name',
             'help' => 'Search by file name (subsring match)'),
@@ -41,6 +42,9 @@ class FileManager extends Module {
         'to' => array('-m', '--to', 'metavar'=>'BK',
             'help' => 'Target backend for migration. See `backends` action
                 for a list of available backends'),
+
+        'file' => array('-f', '--file', 'metavar'=>'FILE',
+            'help' => 'Filename used for import and export'),
 
         'verbose' => array('-v', '--verbose', 'action'=>'store_true',
             'help' => 'Be more verbose'),
@@ -110,9 +114,46 @@ class FileManager extends Module {
             }
             $this->stdout->write("Migrated $count files\n");
             break;
+
+        case 'export':
+            // Create a temporary ZIP file
+            $files = FileModel::objects();
+            $this->_applyCriteria($options, $files);
+
+            if (!$options['file'])
+                $this->fail('Please specify zip file with `-f`');
+
+            $zip = new ZipArchive();
+            if (true !== ($reason = $zip->open($options['file'],
+                    ZipArchive::CREATE)))
+                $this->fail($reason.': Unable to create zip file');
+
+            $manifest = array();
+            foreach ($files as $m) {
+                $f = AttachmentFile::lookup($m->id);
+                $zip->addFromString($f->getId(), $f->getData());
+                $zip->setCommentName($f->getId(), $f->getName());
+                // TODO: Log %attachment and %ticket_attachment entries
+                $info = array('file' => $f->getInfo());
+                foreach ($m->tickets as $t)
+                    $info['tickets'][] = $t->ht;
+
+                $manifest[$f->getId()] = $info;
+            }
+            $zip->addFromString('MANIFEST', serialize($manifest));
+            $zip->close();
+            break;
+
+        case 'expunge':
+            // Create a temporary ZIP file
+            $files = FileModel::objects();
+            $this->_applyCriteria($options, $files);
+
+            foreach ($files as $f)
+                $f->tickets->expunge();
+
+            $files->delete();
         }
-
-
     }
 
     function _applyCriteria($options, $qs) {
@@ -122,7 +163,7 @@ class FileManager extends Module {
             case 'ticket':
                 $qs->filter(array('tickets__ticket_id'=>$val));
                 break;
-            case 'file':
+            case 'file-id':
                 $qs->filter(array('id'=>$val));
                 break;
             case 'name':
@@ -188,6 +229,13 @@ class TicketAttachmentModel extends VerySimpleModel {
                 'constraint' => array('ticket_id' => 'TicketModel.ticket_id'),
             ),
         ),
+    );
+}
+
+class AttachmentModel extends VerySimpleModel {
+    static $meta = array(
+        'table' => ATTACHMENT_TABLE,
+        'pk' => array('object_id', 'type', 'file_id'),
     );
 }
 

setup/inc/class.installer.php
@@ -155,19 +155,19 @@ class Installer extends SetupWizard {
             $i18n->loadDefaultData();
 
             $sql='SELECT `id` FROM '.PREFIX.'sla ORDER BY `id` LIMIT 1';
-            $sla_id_1 = db_result(db_query($sql, false), 0);
+            $sla_id_1 = db_result(db_query($sql, false));
 
             $sql='SELECT `dept_id` FROM '.PREFIX.'department ORDER BY `dept_id` LIMIT 1';
-            $dept_id_1 = db_result(db_query($sql, false), 0);
+            $dept_id_1 = db_result(db_query($sql, false));
 
             $sql='SELECT `tpl_id` FROM '.PREFIX.'email_template_group ORDER BY `tpl_id` LIMIT 1';
-            $template_id_1 = db_result(db_query($sql, false), 0);
+            $template_id_1 = db_result(db_query($sql, false));
 
             $sql='SELECT `group_id` FROM '.PREFIX.'groups ORDER BY `group_id` LIMIT 1';
-            $group_id_1 = db_result(db_query($sql, false), 0);
+            $group_id_1 = db_result(db_query($sql, false));
 
             $sql='SELECT `value` FROM '.PREFIX.'config WHERE namespace=\'core\' and `key`=\'default_timezone_id\' LIMIT 1';
-            $default_timezone = db_result(db_query($sql, false), 0);
+            $default_timezone = db_result(db_query($sql, false));
 
             //Create admin user.
             $sql='INSERT INTO '.PREFIX.'staff SET created=NOW() '
@@ -194,7 +194,7 @@ class Installer extends SetupWizard {
 
 
             $sql='SELECT `email_id` FROM '.PREFIX."email WHERE `email`='alerts@$domain' LIMIT 1";
-            $alert_email_id = db_result(db_query($sql, false), 0);
+            $alert_email_id = db_result(db_query($sql, false));
 
             //Create config settings---default settings!
             //XXX: rename ostversion  helpdesk_* ??

commit c434ccbe5cab3b79ab61e9cf062a2cf1574e4a38

rfc822: Fix validation bug introduced in f99ca0c4
Also add several lint fixes for packaging

include/pear/Mail/RFC822.php
@@ -363,13 +363,11 @@ class Mail_RFC822 {
     function _hasUnclosedQuotes($string)
     {
         $matches = array();
-        if (!preg_match_all('/\\|"/S', $string, $matches, PREG_SET_ORDER))
+        if (!preg_match_all('/[\\"]/', trim($string), $matches))
             return false;
 
-        $string = trim($string);
-        $iMax = strlen($string);
         $in_quote = false;
-        $i = $slashes = 0;
+        $slashes = 0;
 
         foreach ($matches[0] as $m) {
             switch ($m) {

setup/cli/modules/import.php
@@ -266,7 +266,7 @@ class Importer_B extends Importer {
         $this->stderr->write("Importing table: {$header[1]}\n");
 
         $res = db_query("select column_name from information_schema.columns
-            where table_schema=DATABASE() and table_name='$t'");
+            where table_schema=DATABASE() and table_name='{$header[1]}'");
         while (list($field) = db_fetch_row($res))
             if (!in_array($field, $header[2]))
                 $this->fail($header[1]

setup/test/tests/stubs.php
@@ -92,6 +92,8 @@ class Phar {
 class ZipArchive {
     function statIndex() {}
     function getFromIndex() {}
+    function setCommentName() {}
+    function addFromString() {}
 }
 
 class finfo {