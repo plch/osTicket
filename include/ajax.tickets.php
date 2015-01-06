plch/osTicket

commit fcb34cfbb652d882250f625be8909320226ca937

date search fix
This resolves #1570

include/ajax.tickets.php
@@ -191,6 +191,7 @@ class TicketsAjaxAPI extends AjaxController {
         //dates
         $startTime  =($req['startDate'] && (strlen($req['startDate'])>=8))?strtotime($req['startDate']):0;
         $endTime    =($req['endDate'] && (strlen($req['endDate'])>=8))?strtotime($req['endDate']):0;
+		$endTime += (60 * 60 * 24) - 1;	// $endTime should be the last second of the day, not the first like $startTime
         if( ($startTime && $startTime>time()) or ($startTime>$endTime && $endTime>0))
             $startTime=$endTime=0;
 

commit 67fe609e33feecc7c2dfcfb25cefee7e1fdfb5a9

oops: Fix advanced search regression from fcb34cfb
Don't add to endTime if not already set

include/ajax.tickets.php
@@ -191,7 +191,9 @@ class TicketsAjaxAPI extends AjaxController {
         //dates
         $startTime  =($req['startDate'] && (strlen($req['startDate'])>=8))?strtotime($req['startDate']):0;
         $endTime    =($req['endDate'] && (strlen($req['endDate'])>=8))?strtotime($req['endDate']):0;
-		$endTime += (60 * 60 * 24) - 1;	// $endTime should be the last second of the day, not the first like $startTime
+        if ($endTime)
+            // $endTime should be the last second of the day, not the first like $startTime
+            $endTime += (60 * 60 * 24) - 1;
         if( ($startTime && $startTime>time()) or ($startTime>$endTime && $endTime>0))
             $startTime=$endTime=0;
 