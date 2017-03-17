plch/osTicket

commit 71a6b2a0d4419ee8c85724fb382ea51aa409d8af

Update several files for "this ..." translations
Within this commit, several files got updated to split up strings like "Disable for this team" / "Disable for this department" into "Disable for" and "this team"/"this department".
So a) a little bit less translations and b) all strings of "this (ticket|task|API|canned|FAQ|depart|email|group|site|SLA|agent|team|template|help|ban|category|custom|end user|role)" can match already existing "this ..." translations now.
PS: Didn't remove all that whitespaces in include/staff/tpl.inc.php - seems like my php editor app has done that automatically :/

include/staff/templates/task-preview.tmpl.php
@@ -2,7 +2,7 @@
 $error=$msg=$warn=null;
 
 if (!$task->checkStaffPerm($thisstaff))
-     $warn.= __('You do not have access to this task');
+     $warn.= __('You do not have access to %s'), __('this task');
 elseif ($task->isOverdue())
     $warn.='&nbsp;<span class="Icon overdueTicket">'.__('Marked overdue!').'</span>';
 

include/class.staff.php
@@ -1090,7 +1090,7 @@ implements AuthenticatedUser, EmailContact, TemplateVariable {
             if (!$dept_id || !Dept::lookup($dept_id))
                 $errors['dept_access'][$dept_id] = __('Select a valid department');
             if ($dept_id == $this->getDeptId())
-                $errors['dept_access'][$dept_id] = __('Agent already has access to this department');
+                $errors['dept_access'][$dept_id] = sprintf(__('Agent already has access to %s'), __('this department'));
             $da = $this->dept_access->findFirst(array('dept_id' => $dept_id));
             if (!isset($da)) {
                 $da = new StaffDeptAccess(array(

include/class.ticket.php
@@ -3518,7 +3518,7 @@ implements RestrictedAccess, Threadable {
             && ($role = $thisstaff->getRole($vars['deptId']))
             && !$role->hasPerm(TicketModel::PERM_CREATE)
         ) {
-            $errors['err'] = __('You do not have permission to create a ticket in this department');
+            $errors['err'] = sprintf(__('You do not have permission to create a ticket in %s'), __('this department'));
             return false;
         }
 

include/client/view.inc.php
@@ -6,7 +6,7 @@ $info=($_POST && $errors)?Format::htmlchars($_POST):array();
 $dept = $ticket->getDept();
 
 if ($ticket->isClosed() && !$ticket->isReopenable())
-    $warn = __('This ticket is marked as closed and cannot be reopened.');
+    $warn = sprintf(__('%s is marked as closed and cannot be reopened.'), __('This ticket'));
 
 //Making sure we don't leak out internal dept names
 if(!$dept || !$dept->isPublic())

include/staff/department.inc.php
@@ -227,7 +227,7 @@ $info = Format::htmlchars(($errors && $_POST) ? $_POST : $info);
                 <label>
                 <input type="checkbox" name="ticket_auto_response" value="0" <?php echo !$info['ticket_auto_response']?'checked="checked"':''; ?> >
 
-                <?php echo __('<strong>Disable</strong> for this Department'); ?>
+                <?php echo sprintf(__('<strong>Disable</strong> for %s'), __('this department')); ?>
                 </label>
                 <i class="help-tip icon-question-sign" href="#new_ticket"></i>
             </td>
@@ -239,7 +239,7 @@ $info = Format::htmlchars(($errors && $_POST) ? $_POST : $info);
             <td>
                 <label>
                 <input type="checkbox" name="message_auto_response" value="0" <?php echo !$info['message_auto_response']?'checked="checked"':''; ?> >
-                <?php echo __('<strong>Disable</strong> for this Department'); ?>
+                <?php echo sprintf(__('<strong>Disable</strong> for %s'), __('this department')); ?>
                 </label>
                 <i class="help-tip icon-question-sign" href="#new_message"></i>
             </td>
@@ -322,7 +322,7 @@ $info = Format::htmlchars(($errors && $_POST) ? $_POST : $info);
             <td colspan="2">
                 <?php echo __('Department Members'); ?>
                 <div><small>
-                <?php echo __('Agents who are primary members of this department'); ?>
+                <?php echo sprintf(__('Agents who are primary members of %s'), __('this department')); ?>
                 </small></div>
             </td>
         </tr>

include/staff/email.inc.php
@@ -152,7 +152,7 @@ $info=Format::htmlchars(($errors && $_POST)?$_POST:$info);
             </td>
             <td>
                 <label><input type="checkbox" name="noautoresp" value="1" <?php echo $info['noautoresp']?'checked="checked"':''; ?> >
-                <?php echo __('<strong>Disable</strong> for this Email Address'); ?>
+                <?php echo sprintf(__('<strong>Disable</strong> for %s'), __('this email')); ?>
                 </label>
                 <i class="help-tip icon-question-sign" href="#auto_response"></i>
             </td>
@@ -313,7 +313,7 @@ $info=Format::htmlchars(($errors && $_POST)?$_POST:$info);
             <td><?php echo __('Header Spoofing'); ?></td>
             <td>
                 <label><input type="checkbox" name="smtp_spoofing" value="1" <?php echo $info['smtp_spoofing'] ?'checked="checked"':''; ?>>
-                <?php echo __('Allow for this Email Address'); ?></label>
+                <?php echo sprintf(__('Allow for %s'), __('this email')); ?></label>
                 <i class="help-tip icon-question-sign" href="#header_spoofing"></i>
             </td>
         </tr>

include/staff/faq.inc.php
@@ -77,7 +77,7 @@ if ($topics = Topic::getAllHelpTopics()) {
 ?>
     <div style="padding-top:9px">
         <strong><?php echo __('Help Topics');?></strong>:
-        <div class="faded"><?php echo __('Check all help topics related to this FAQ.');?></div>
+        <div class="faded"><?php echo sprintf(__('Check all help topics related to %s.'), __('this FAQ article'));?></div>
     </div>
     <select multiple="multiple" name="topics[]" class="multiselect"
         data-placeholder="<?php echo __('Help Topics'); ?>"

include/staff/team.inc.php
@@ -103,7 +103,7 @@ $info = $team->getInfo();
             </td>
             <td>
                 <input type="checkbox" name="noalerts" value="1" <?php echo !$team->alertsEnabled()?'checked="checked"':''; ?> >
-                <?php echo __('<strong>Disable</strong> for this Team'); ?>
+                <?php echo sprintf(__('<strong>Disable</strong> for %s'), __('this team')); ?>
                 <i class="help-tip icon-question-sign" href="#assignment_alert"></i>
             </td>
         </tr>
@@ -135,7 +135,7 @@ foreach ($members as $m)
             <td colspan="2">
                 <?php echo __('Team Members'); ?>
                 <div><small>
-                <?php echo __('Agents who are members of this team'); ?>
+                <?php echo sprintf(__('Agents who are members of %s'), __('this team')); ?>
                 <i class="help-tip icon-question-sign" href="#members"></i>
                 </small></div>
             </td>

commit 2480452de4983b4b88b9ef5f3862cdf87691f7a5

Syntax Errors Hotfix
Fixes syntax error introduced with commit 71a6b2a0 & 6e0ddf2e

include/ajax.tasks.php
@@ -689,7 +689,7 @@ class TasksAjaxAPI extends AjaxController {
                 $errors['err'] = $info['error'] = $m;
             else
                 $info['warn'] = sprintf(__('Are you sure you want to change status of %s?'),
-                        sprintf(__('this task'));
+                        __('this task'));
             break;
         default:
             Http::response(404, __('Unknown status'));

include/staff/templates/task-preview.tmpl.php
@@ -2,7 +2,7 @@
 $error=$msg=$warn=null;
 
 if (!$task->checkStaffPerm($thisstaff))
-     $warn.= __('You do not have access to %s'), __('this task');
+     $warn.= sprintf(__('You do not have access to %s'), __('this task'));
 elseif ($task->isOverdue())
     $warn.='&nbsp;<span class="Icon overdueTicket">'.__('Marked overdue!').'</span>';
 